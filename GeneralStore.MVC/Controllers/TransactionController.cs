22f985b7a0b2803590633a1aae0d1638f40704d7
mobara121 <mizueo121@gmail.com>
2020-09-29 13:29:29 -0400

    Customer completed

---
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Transaction
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [ForeignKey(nameof(Customer))]  // or [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+       
+        
+        public int ProductId { get; set; }
+        
+        [Display(Name = "Product ID")]
+        [ForeignKey(nameof(Product))] // or [ForeignKey("Product")]
+        public virtual List<Product> ProductList { get; set; }
+
+
+
+        [Required]
+        public int ProductPurchased { get; set; }
+
+        //[Required]
+        public DateTime DateOfTransaction { get; set; }
+
+    }
+}
